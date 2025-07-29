7893539729e0c429d8517f77ec67cf9f85d6c97a
Filipe Braga de Menezes <flipebgmufp@gmail.com>
2025-07-29 18:01:08 -0300

    Correção e adição de funcionalidades em Turno.java

--- src/main/java/br/com/seuusuario/jogorpg/core/Turno.java
+++ src/main/java/br/com/seuusuario/jogorpg/core/Turno.java
@@ -1,59 +1,74 @@
 package nucleo;
-
 import Player.Player;
 import Player.Hero;
-import log.Log;
-import enums.TipoLog;
-import enums.ResultadoAtaque;
-import core.SistemaCombate;
+import monstros.Monster;
 
 import java.util.*;
 
+
 public class Turno {
     private int rodada;
-    private List<Player> jogadores;
+    public List<Player> jogadores;
     private List<Player> ordemTurno;
     private int acoesRealizadas;
 
-    public Turno(int rodada, List<Player> jogadores) {
+    public Turno(int rodada, List<Player> jogadores, Game game) {
         this.rodada = rodada;
         this.jogadores = jogadores;
         this.ordemTurno = ordenarPorVelocidade();
         this.acoesRealizadas = 0;
+
+        List<Hero> herois = game.getHerois();
+        List<Monster> monstros = game.getMonstros();
     }
 
-    public void iniciarTurno() {
+    public void iniciarTurno(Game game) {
         acoesRealizadas = 0;
         System.out.println("===========================================");
         System.out.printf("          %d° rodada iniciada\n", rodada++);
         System.out.println("===========================================");
 
-        while (!verificarFimTurno()) {
-            Player jogador = ordemTurno.get(acoesRealizadas);
+        while (!verificarFimdoTurno()) {
+            Player jogador = ordemTurno.get(acoesRealizadas); // Jogador que vai agir no turno
+            System.out.println("\n");
+            System.out.println(jogador.getNome() + " está agindo...");
 
-            if (jogador.getHp() <= 0) {
-                acoesRealizadas++;
-                continue;
-            }
-
+            // Verifica o tipo do jogador (Hero ou Monster)
             if (jogador instanceof Hero) {
-                executarAcaoJogador(jogador);
-            } else {
-                processarAcoesMonstros(jogador);
+                // Herói ataca um monstro
+                for (Monster monstro : game.getMonstros()) {
+                    if (monstro.getHp() > 0) {
+                        int dano = 10;
+                        monstro.setHp(monstro.getHp() - dano);
+                        System.out.println(jogador.getNome() + " atacou " + monstro.getNome() + " causando " + dano + " de dano\n!");
+                        break; // Sai do loop após atacar o primeiro monstro
+                    }
+                }
+            } else if (jogador instanceof Monster) {
+                // Monstro ataca um herói
+                for (Hero heroi : game.getHerois()) {
+                    if (heroi.getHp() > 0) {
+                        int dano = 10;
+                        heroi.setHp(heroi.getHp() - dano);
+                        System.out.println(jogador.getNome() + " atacou " + heroi.getNome() + " causando " + dano + " de dano!");
+                        break; // Sai do loop após atacar o primeiro herói
+                    }
+                }
             }
 
-            acoesRealizadas++;
+            // Exibe as estatísticas após a ação do jogador
+            for (Map.Entry<String, Player> entry : game.getEstatisticasBatalha().entrySet()) {
+                Player p = entry.getValue();
+                System.out.println(p.getNome() + " - HP: " + p.getHp() + ", Velocidade: " + p.getVelocidade());
+            }
+
+            acoesRealizadas++;  // Incrementa o contador de ações realizadas
+
+            // Verifica se todos os jogadores já agiram
+            if (verificarFimdoTurno()) {
+                System.out.println("Turno concluído.");
+            }
         }
-
-        System.out.println("Turno concluído.");
-    }
-
-    public void executarAcaoJogador(Player heroi) {
-        // TODO: lógica de IA feita pelo Aldrey
-    }
-
-    public void processarAcoesMonstros(Player monstro) {
-        // TODO: lógica de IA feita pelo Aldrey
     }
 
     public List<Player> ordenarPorVelocidade() {
@@ -67,14 +82,26 @@ public class Turno {
         return ordenada;
     }
 
-    public boolean verificarFimTurno() {
+    public void executarAcaoJogador() {
+        //Irá entrar o código do Aldrey
+
+    }
+
+    public boolean verificarFimdoTurno() {
         return acoesRealizadas >= ordemTurno.size();
+
     }
 
     public List<Player> getOrdemTurno() {
-        System.out.println("A ordem dos ataques dos hérois é: " );
-        for(Player ordemTurno : ordemTurno){
-            System.out.println(ordemTurno.getNome());}
+        System.out.println("A ordem dos ataques dos hérois é: ");
+        for (Player ordemTurno : ordemTurno) {
+            System.out.println(ordemTurno.getNome());
+        }
         return ordemTurno;
+    }
+
+    public void processarAcoesMonstros() {
+        //Irá entrar o código do Aldrey
+
     }
 }
